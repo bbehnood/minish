3c247590ecd8584b81263d8bdecda7458ad38c9f
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-05-29 12:41:24 +0330

    Implement token functions

---
+++ src/token.c
@@ -0,0 +1,54 @@
+#include "token.h"
+#include <stdlib.h>
+#include <string.h>
+
+token_t *new_token(token_type_t type, char *value)
+{
+    token_t *token = (token_t *)malloc(sizeof(token_t));
+    if (!token)
+        return NULL;
+
+    token->type = type;
+
+    if (value)
+        token->value = strdup(value);
+    else
+        token->value = NULL;
+
+    token->next = NULL;
+
+    return token;
+}
+
+void append_token(token_t **head, token_t *new)
+{
+    token_t *tmp;
+
+    if (!*head)
+    {
+        *head = new;
+        return;
+    }
+
+    tmp = *head;
+
+    while (tmp->next)
+        tmp = tmp->next;
+
+    tmp->next = new;
+}
+
+void free_tokens(token_t *head)
+{
+    token_t *tmp;
+
+    while (head)
+    {
+        tmp = head->next;
+
+        free(head->value);
+        free(head);
+
+        head = tmp;
+    }
+}
