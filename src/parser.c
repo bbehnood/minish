eb8080e7a28c69ad59d611dda83478d5cfbff933
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-06-01 22:04:12 +0330

    feat: Implement helper syntax validation functions

--- src/parser.c
+++ src/parser.c
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include "token.h"
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,4 +13,42 @@ static void parser_init(parser_t *parser, token_t *tokens)
 static void parser_advance(parser_t *parser)
 {
     parser->current = parser->current->next;
+}
+
+static void syntax_error(const char *token)
+{
+    fprintf(stderr, "minish: syntax error near `%s`\n", token);
+}
+
+static int validate_syntax(token_t *tokens)
+{
+    if (tokens->type == TOKEN_PIPE)
+    {
+        syntax_error(tokens->value);
+        return -1;
+    }
+
+    tokens = tokens->next;
+
+    while (tokens)
+    {
+        if (tokens->type == TOKEN_PIPE)
+        {
+            if (!tokens->next)
+            {
+                syntax_error(tokens->value);
+                return -1;
+            }
+
+            if (tokens->next && tokens->next->type == TOKEN_PIPE)
+            {
+                syntax_error(tokens->value);
+                return -1;
+            }
+        }
+
+        tokens = tokens->next;
+    }
+
+    return 0;
 }
