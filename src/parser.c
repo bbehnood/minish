aeaf977a0404b1d59e9d0698de980de3aff973a0
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-06-01 22:51:30 +0330

    feat: Implement helper function for parsing a single command

--- src/parser.c
+++ src/parser.c
@@ -52,3 +52,66 @@ static int validate_syntax(token_t *tokens)
 
     return 0;
 }
+
+static int count_words(token_t *tokens)
+{
+    int count = 0;
+
+    while (tokens && tokens->type == TOKEN_WORD)
+    {
+        tokens = tokens->next;
+        count++;
+    }
+
+    return count;
+}
+
+static command_t *parse_command(parser_t *parser)
+{
+    command_t *cmd;
+    int argc;
+    int i;
+
+    argc = count_words(parser->current);
+
+    if (argc == 0)
+        return NULL;
+
+    cmd = (command_t *)malloc(sizeof(command_t));
+    if (!cmd)
+    {
+        fprintf(stderr, "minish: out of memory\n");
+        return NULL;
+    }
+
+    cmd->argv = (char **)malloc(sizeof(char *) * (argc + 1));
+    if (!cmd->argv)
+    {
+        fprintf(stderr, "minish: out of memory\n");
+        free(cmd);
+        return NULL;
+    }
+
+    i = 0;
+
+    while (parser->current && parser->current->type == TOKEN_WORD)
+    {
+        cmd->argv[i] = strdup(parser->current->value);
+        if (!cmd->argv[i])
+        {
+            while (i > 0)
+            {
+                i--;
+                free(cmd->argv[i]);
+                return NULL;
+            }
+        }
+
+        i++;
+        parser_advance(parser);
+    }
+
+    cmd->argv[i] = NULL;
+
+    return cmd;
+}
