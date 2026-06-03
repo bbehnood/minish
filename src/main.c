f76f9444e721df3776a011509b8e505433f87351
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-06-03 11:01:22 +0330

    feat: Migrate from getline to readline

--- src/main.c
+++ src/main.c
@@ -5,25 +5,31 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
 
 #define C_RESET "\033[0m"
 #define C_GREEN "\033[32m"
 #define C_BLUE "\033[34m"
 
+#define PROMPT_SIZE 1024
+
 int main(int argc, char **argv, char **envp)
 {
     shell_t shell;
     size_t len = 0;
+    char prompt[PROMPT_SIZE];
 
     shell_init(&shell, envp);
 
     while (shell.running)
     {
-        printf("%s%s@%s%s:%s%s%s$ ", C_GREEN, shell.username, shell.hostname,
-               C_RESET, C_BLUE, shell.cwd, C_RESET);
+        snprintf(prompt, sizeof(prompt), "%s%s@%s%s:%s%s%s$ ", C_GREEN,
+                 shell.username, shell.hostname, C_RESET, C_BLUE, shell.cwd,
+                 C_RESET);
         fflush(stdout);
 
-        if (getline(&shell.input_line, &len, stdin) == -1)
+        shell.input_line = readline(prompt);
+        if (!shell.input_line)
         {
             printf("\n");
             break;
